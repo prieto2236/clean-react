c4e1fdee3de797cc4d5716c0f3a53e8e80d87bb9
André Prieto <prieto2236@gmail.com>
2022-01-21 23:58:40 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,30 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extentions: ['.ts', '.tsx', '.js'],
+    extentions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'scss-loader'
+      }],
+      exclude: /node_modules/
+    }]
   },
   devServer: {
     contentBase: './public',
