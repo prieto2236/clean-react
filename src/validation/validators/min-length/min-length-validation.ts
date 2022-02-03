6e3e94b213b3182e0068d2a73774c0920b4cf1b5
André Prieto <prieto2236@gmail.com>
2022-02-03 00:54:45 -0300

    fix: correction of class name on MinLengthValidation

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,7 +1,7 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
-export class MinLenthValidation implements FieldValidation {
+export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) { }
 
   validate (value: string): Error {
