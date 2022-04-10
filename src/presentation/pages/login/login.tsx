5ca16d8e76ee6a0727a2408d658d8c8f961f7c27
André Prieto <prieto2236@gmail.com>
2022-04-09 22:00:00 -0300

    fix: change input type to email on Login view

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -56,7 +56,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
       <Context.Provider value={{ state, setState }}>
         <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
-          <Input type="text" name="email" placeholder="Digite seu e-mail" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Entrar</button>
           <Link data-testid="signup" to="/signup" className={Styles.link}>Criar conta</Link>
