3d029e2d2d5c89e060662f6648242e85be94eb10
André Prieto <prieto2236@gmail.com>
2022-04-12 17:45:01 -0300

    feat: ensure Login link goes to the right page

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -4,7 +4,7 @@ import Styles from './signup-styles.scss'
 import React, { useEffect, useState } from 'react'
 import { Validation } from '@/presentation/protocols/validation'
 import { AddAccount, SaveAccessToken } from '@/domain/usecases'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 type Props = {
   validation: Validation
@@ -71,7 +71,7 @@ const SignUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Pr
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
           <button data-testid="submit" disabled={!!state.nameError || !!state.emailError || !!state.passwordError || !!state.passwordConfirmationError} className={Styles.submit} type="submit">Entrar</button>
-          <span className={Styles.link}>Voltar</span>
+          <Link data-testid="login-link" replace to="/login" className={Styles.link}>Voltar</Link>
           <FormStatus />
         </form>
       </Context.Provider>
