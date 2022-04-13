fb4fd91f84ff6f5a00d9fa629dd9174b3ac6f107
André Prieto <prieto2236@gmail.com>
2022-04-13 14:02:17 -0300

    chore: add cypress command

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,8 @@
 describe('', () => {
-  it('Shoud load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Shoud load with correct initial state', () => {
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
   })
 })
